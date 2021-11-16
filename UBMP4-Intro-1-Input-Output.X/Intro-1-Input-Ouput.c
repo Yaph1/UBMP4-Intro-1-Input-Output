5ac8c5a5bfd85162ddeaa3f1d4d73c73a7f85b4d
yaphi30@icloud.com <yaphi30@icloud.com>
2021-11-16 18:41:07 +0000

    .

--- UBMP4-Intro-1-Input-Output.X/Intro-1-Input-Ouput.c
+++ UBMP4-Intro-1-Input-Output.X/Intro-1-Input-Ouput.c
@@ -105,10 +105,10 @@ int main(void)
  * 1. How many times do the LEDs flash if SW2 is quickly pressed and released?
  *    Do the LEDs keep flashing when SW2 is held? Look at the program and
  *    explain why this happens when SW2 is held.
- The LED's only flash if SW2 is pressed, and will flash for as long as it is held.
+ The LED's only flash once if SW2 is pressed, but will flash for as long as it is held.
  * 
- * 2. Explain the difference between the statements: LED3 = 0; and LED3 = 1;
- LED3 = 0 outputs 0V to the LED. LED3 = 2 outpits 5V to the LED.
+ * 2. Explain the difference between the statements: LED3 = 0; and LED3 = 1; 
+ LED3 = 0 outputs 0V to the LED. LED3 = 1 outpits 5V to the LED.
  * 
  * 3. What voltage do you expect the microcontroller to output to LED D3 when
  *    the statement LED3 = 0; runs? What voltage do you expect the output to be
@@ -118,7 +118,7 @@ int main(void)
  *    You can confirm the output voltage with a voltmeter if you have access
  *    to one. If you tried that, did the voltage match your prediction?
  * 
- * 4. The statement 'if(SW2 == 0)' uses two equal signs, while the statement
+ * 4. The statement 'if(SW2 == 0)' uses two equal signs, while the statementnj      
  *    'LED3 = 1;' uses a single equal sign. What operation is performed by one
  *    equal sign? What operation is performed by two equal signs?
  An assignment operation is performed by one equal sign, and a conditional operation is performed by two equal signs.
